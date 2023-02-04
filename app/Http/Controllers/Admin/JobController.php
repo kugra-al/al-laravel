5980435bd12c7c38ca372a463a3408dba9b014d5
kugra <kugra@accursed-lands.net>
2023-02-04 17:49:35 +0200

    Clean jobcontroler and expand fetchgithubfiles job

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -5,20 +5,33 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use App\Jobs\FetchGithubFiles;
 
 class JobController extends Controller
 {
     public function index()
     {
+        // Don't do this, we know what the jobs will be
         $jobs_path = public_path()."/../app/Jobs/";
-        $jobs = [];
-        foreach(File::allFiles($jobs_path) as $job) {
-            $jobs[] = basename($job);
-        }
+        $jobs = ['fetch-item-files'];
         return view('admin.jobs',["jobs"=>$jobs]);
     }
 
-    public function runJob() {
-        return "ok, running job";
+    public function runJob(Request $request) {
+        $job = $request->job;
+        $status = [];
+        switch($job) {
+            case "fetch-item-files" :
+                $repo = "Amirani-al/Accursedlands-obj";
+                $directory = "obj/items/";
+                $extensions = [".itm"];
+                $skipdirs = ["bak","old"];
+                FetchGithubFiles::dispatch($repo, $directory, $extensions, $skipdirs);
+                $status = ["success"=>"running job: $job"];
+                break;
+            default :
+                $status = ["errors"=>"unknown job: $job"];
+        }
+        return json_encode($status);
     }
 }
--- app/Jobs/FetchGithubFiles.php
+++ app/Jobs/FetchGithubFiles.php
@@ -8,19 +8,36 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class FetchGithubFiles implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $repo, $directory, $extensions, $skipdirs;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($repo, $directory, $extensions, $skipdirs)
     {
-        //
+        $this->repo = $repo;
+        $this->directory = $directory;
+        $this->extensions = $extensions;
+        $this->skipdirs = $skipdirs;
+
+        $process = new Process(['git clone ', '--with-token '.env('GITHUB_TOKEN'), $repo]);
+        $process->setWorkingDirectory(storage_path()."/private/git/");
+        $process->run();
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        dd( $process->getOutput() );
+        dd($process);
+        dd($this);
     }
 
     /**
