73e3b1a1ea919b5dd31a05a3e0919826e5859ad8
kugra <kugra@accursed-lands.net>
2023-02-11 14:16:59 +0200

    Convert existing users to git if login exists

--- app/Http/Controllers/GitHubOAuthController.php
+++ app/Http/Controllers/GitHubOAuthController.php
@@ -31,26 +31,33 @@ class GitHubOAuthController extends Controller
                 return redirect('/home');
 
             }else{
-                if (!$user->name)
-                    $user->name = explode("@",$user->email)[0];
-                $gitUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'github_id'=> $user->id,
-                    'auth_type'=> 'github',
-                    'password' => \Hash::make(\Str::random(12));
-                ]);
-
+                $status = "Created new user account";
+                // Check for existing user and convert to gituser
+                $gitUser = User::where('email',$user->email)->first();
+                if ($gitUser) {
+                    $gituser->auth_type = 'github';
+                    $gitUser->github_id = $user->id;
+                    $gitUser->save();
+                    $status = "Converted user login to github login";
+                } else {
+                    if (!$user->name)
+                        $user->name = explode("@",$user->email)[0];
+                    $gitUser = User::create([
+                        'name' => $user->name,
+                        'email' => $user->email,
+                        'github_id'=> $user->id,
+                        'auth_type'=> 'github',
+                        'password' => \Hash::make(\Str::random(12));
+                    ]);
+                }
                 Auth::login($gitUser);
 
-                return redirect('/home');
+                return redirect('/home')->with('status',$status);
             }
 
         } catch (Exception $e) {
             Log::error($e->getMessage());
-            return \Redirect('/login')->with('status',"Something went wrong. Error logged");
-//            dd("Something went wrong. Error logged");
-//            dd($e->getMessage());
+            return \Redirect('/login')->with('error',"Something went wrong. Error logged");
         }
     }
 }
